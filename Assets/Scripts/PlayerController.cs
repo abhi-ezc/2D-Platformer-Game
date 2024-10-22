4406795ef346fc0e53855163d2c709e5920e6172
Abhinav MK <abhinav.mk@gravityilabs.com>
2024-10-22 23:02:45 +0530

    [Task 1] - Make crouch animation and on pressing Cntrl key on keyboard, player crouches and resize the collider.
    [Task 2] - Accept the vertical in the same way as we accepted the horizontal input and when vertical > 0 then play jump animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider2D;
      
-
-    private static readonly int Velocity = Animator.StringToHash("Velocity");
-
+    // Animator Parameter Keys
+    private static readonly int VelocityAnimatorKey = Animator.StringToHash("Velocity");
+    private static readonly int IsCrouchingAnimatorKey = Animator.StringToHash("IsCrouching");
+    private static readonly int JumpAnimatorKey = Animator.StringToHash("Jump");
+    
+    // Box Collider Sizes and Offset
+    private readonly Rect FullHeightColliderMetrics = new Rect(new Vector2(0f, 1f), new Vector2(0.5f, 2f));
+    private readonly Rect CrouchedHeightColliderMetrics = new Rect(new Vector2(-0.13f, 0.6f),new Vector2(0.9f,1.31f));
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +25,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateVelocity();
+        CheckIfCrouching();
+        CheckIfJumping();
+    }
+
+    private void UpdateVelocity()
+    {
         float speed = Input.GetAxis("Horizontal");
-        animator.SetFloat(Velocity,Mathf.Abs(speed));
+        animator.SetFloat(VelocityAnimatorKey, Mathf.Abs(speed));
         if (speed < 0)
         {
             spriteRenderer.flipX = true;
@@ -30,10 +44,42 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            Debug.Log(speed);
             // do nothing
             // it will preserve the last flipx
             // if no movement the player sprite will look at the last direction
+        }
+    }
+
+    private void CheckIfCrouching()
+    {
+        bool isCrouching = !animator.GetBool(IsCrouchingAnimatorKey);
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (isCrouching)
+            {
+                boxCollider2D.offset = CrouchedHeightColliderMetrics.position;
+                boxCollider2D.size = CrouchedHeightColliderMetrics.size;
+            }
+            else
+            {
+                boxCollider2D.offset = FullHeightColliderMetrics.position;
+                boxCollider2D.size = FullHeightColliderMetrics.size;
+            }
+            animator.SetBool(IsCrouchingAnimatorKey,isCrouching);
+        }
+    }
+
+    private void CheckIfJumping()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical > 0)
+        {
+            animator.SetBool(IsCrouchingAnimatorKey, false);
+            animator.SetTrigger(JumpAnimatorKey);
+        }
+        else
+        {
+            animator.ResetTrigger(JumpAnimatorKey);
         }
     }
 

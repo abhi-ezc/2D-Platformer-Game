d52562dda5b3472c6853b6622f6f66a9b3609df3
Abhinav MK <abhinav.mk@gravityilabs.com>
2024-10-27 13:46:45 +0530

    Build a Game Over UI with a Restart button on Player Death

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,37 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+
     }
 
-    public void OnRestart()
+    public void OnRestart ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    private void OnSceneUnloadCompleted(AsyncOperation asyncOperation)
+    void OnSceneUnloadCompleted (AsyncOperation asyncOperation)
     {
     }
 
-    public void OnQuit()
+    public void OnQuit ()
     {
-        #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-        #endif
-                Application.Quit();
+        SceneManager.LoadScene(0);
     }
 }
